e858400ee522b8ec91e04b6abee44d445ab598a4
EpiCanard <epicanard@gmail.com>
2023-06-27 20:16:52 +0200

    Fix message still displayed with Deluxechat

--- src/main/java/fr/freebuild/shutup/ShutUp.java
+++ src/main/java/fr/freebuild/shutup/ShutUp.java
@@ -20,16 +20,16 @@ public class ShutUp extends JavaPlugin implements Listener {
     this.logger.setDebug(getConfig().getBoolean("debug", false));
 
     getCommand("shutup").setExecutor(this);
-    
+
     Bukkit.getServer().getPluginManager().registerEvents(this, this);
   }
-  
+
   public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
     this.logger.debug("Execute command /shutup reload");
-    
+
     if (sender.isOp()) {
       this.logger.debug("Player is op");
-    
+
       if (args.length > 0 && args[0].equals("reload")){
         this.logger.info("Reloading the config...", sender);
         this.reloadConfig();
@@ -44,18 +44,19 @@ public class ShutUp extends JavaPlugin implements Listener {
       this.logger.debug("Player is NOT op");
       sender.sendMessage("You don't have permission to do that !");
     }
-    
+
     return false;
   }
-  
-  @EventHandler(priority=EventPriority.HIGHEST)
-  public void onPlayerSendChatMessage(final AsyncPlayerChatEvent apce) {
+
+  @EventHandler(priority=EventPriority.LOW)
+  public void onPlayerSendChatMessage(final AsyncPlayerChatEvent event) {
     this.logger.debug("Player trying to send message in chat");
-    final Player player = apce.getPlayer();
+    final Player player = event.getPlayer();
 
     if (player != null && player.hasPermission("shutup") && !player.isOp()) {
       this.logger.debug("The player is not allowed to send message in chat");
-      apce.setCancelled(true);
+      event.setMessage(null);
+      event.setCancelled(true);
       player.sendMessage(ChatColor.RED + this.getConfig().getString("message", "Not allowed"));
     }
   }
